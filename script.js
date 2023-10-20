8bd649185b502dff281bfe7c913a8eb84ecc067e
Pedro Henrique <pedro.h@coalize.com.br>
2023-10-20 15:54:18 -0300

    dada

---
+++ script.js
@@ -0,0 +1,18 @@
+function animateSaldo() {
+  const saldoElement = document.getElementById("saldo");
+  const finalSaldo = 25.42;
+  let currentSaldo = 18.27;
+  const animationSpeed = 0.1; // Adjust this value to control the animation speed.
+
+  function updateSaldo() {
+    if (currentSaldo < finalSaldo) {
+      currentSaldo += animationSpeed;
+      saldoElement.innerText = currentSaldo.toFixed(2);
+      requestAnimationFrame(updateSaldo);
+    }
+  }
+
+  updateSaldo();
+}
+
+animateSaldo();
